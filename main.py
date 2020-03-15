77106aacfe910ad3cded5a062b09cf2cb6a3208c
xuhang <1564285867@qq.com>
2020-03-15 17:06:37 +0800

    消息提示框，自动关闭以及动态添加，更新上传记录

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-from flask import Flask,url_for,request,render_template
+from flask import Flask,redirect,url_for,request,render_template
 import os
 import io
 import addMessage
 app = Flask(__name__)
 userNumber={}
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+userNumber['未知用户'] = 1
+number = 0
 @app.route('/load', methods=['GET', 'POST'])
 def writeEdit():
     # root_dir = os.path.abspath(os.path.dirname(__file__))
     # img_path = root_dir+'\static'+'\img'
     # figfile = io.BytesIO(open(img_path, 'rb').read())
     # img = base64.b64encode(figfile.getvalue()).decode('ascii')
-    number = 43
+    
     if request.method == 'POST':
         userdata= request.json
         print(userdata)
@@ -23,10 +22,13 @@ def writeEdit():
         addMessage.addMessage(userdata)
         userNumber[username] = userNumber[username] +1;
         print("即将返回。。。。。")
-        return render_template('load.html',number=userNumber[username])
+        print(userNumber[username])
+        return str(userNumber[username])
     elif request.method == 'GET':
         return render_template('load.html',number=number)
         #return render_template('load.html',img=img)
-
+@app.route('/')
+def hello_world():
+    return render_template('load.html',number=number)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1',port=80,debug=True)
